7bd12565ff8179bd68b61fa871fd48d49ed3d3b9
Wei Wu <60204220+weiwu1994@users.noreply.github.com>
2020-07-27 17:01:13 -0400

    fix package naming

--- src/main/java/com/weiwudev/EurekaServerApplication.java
+++ src/main/java/com/weiwudev/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.revature;
+package com.weiwudev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/weiwudev/EurekaServerApplicationTests.java
+++ src/test/java/com/weiwudev/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.revature;
+package com.weiwudev;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
